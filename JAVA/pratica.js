8a9d41fe1239dcdd4fbbb2e747896eddb3a34926
CristinaParralesT <parralescristina3@gmail.com>
2025-08-25 20:04:24 -0500

    Eliminar archivos y carpetas que no pertenecen al proyecto landing page

--- JAVA/pratica.js
+++
@@ -1,9 +0,0 @@
-function saludar(nombre) {
-    return "Hola, " + nombre;
-}
-console.log("Ejecuto SALUDAR", saludar("Carlos"))
-
-function despedir(nombre) {
-    return "Adios" ;
-}
-console.log("Ejecuto DESPEDIR" despedir())
